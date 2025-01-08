c15c2ea61ef4e99e24f802ad48d81c5d3fc40a00
Marcio Carneiro <marcioscar@gmail.com>
2025-01-08 17:23:15 -0300

    final

--- clube.py
+++ clube.py
@@ -1,18 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[91]:
+
 
 import pandas as pd
 import streamlit as st
-
 st.set_page_config(layout="wide")
-df_strava = pd.read_csv('atividades_unicas.csv')
+
+
+# In[30]:
+
+
+import paramiko
+from scp import SCPClient
+import pandas as pd
+
+# Configurações do servidor remoto
+host = "145.223.30.123"
+port = 22  # Porta SSH padrão
+username = "root"
+password = "Marcio@aynrand7"
+remote_path = "/root/play/stravaapi/atividades_unicas.csv"
+local_path = "atividades_unicas.csv"
+
+def download_csv_via_scp():
+    # Cria o cliente SSH
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        # Conecta ao servidor
+        ssh.connect(host, port=port, username=username, password=password)
+
+        # Cria o cliente SCP
+        with SCPClient(ssh.get_transport()) as scp:
+            print("Baixando arquivo...")
+            scp.get(remote_path, local_path)  # Baixa o arquivo
+            print(f"Arquivo baixado para {local_path}")
+    except Exception as e:
+        print(f"Erro ao transferir arquivo: {e}")
+    finally:
+        ssh.close()
+
+def process_csv():
+    try:
+        # Lê o arquivo CSV local
+        df = pd.read_csv(local_path)
+        # Exibe as primeiras linhas do DataFrame
+    except Exception as e:
+        print(f"Erro ao processar o arquivo CSV: {e}")
+
+# Executa as funções
+download_csv_via_scp()
+process_csv()
+
+
+# In[35]:
+
+
+df_strava = pd.read_csv('unique_activities_old.csv')
+# df_strava = pd.read_csv('atividades_unicas.csv')
 df_strava['athlete.fullname'] = df_strava['firstname'] + ' ' + df_strava['lastname']
 df_strava['moving_time_minutes'] = round(df_strava['moving_time']/60, 2)
 
+
+# In[94]:
+
+
+from datetime import datetime, timedelta
 
 
 cols = ['athlete.fullname','name', 'type', 'distance',   
          'data_atual', 'distance_km', 'pace_real','moving_time_minutes'
        ]
-df_strava['moving_time_minutes'] = round(df_strava['moving_time']/60, 2)
+# df_strava['moving_time_minutes'] = round(df_strava['moving_time']/60, 2)
 
 df_strava['distance_km'] = round(df_strava['distance'] / 1000, 2)
 
@@ -34,10 +96,61 @@ df_strava['avg_speed_kmh'] = pd.to_numeric(df_strava['avg_speed_kmh'], errors='c
 df_strava['pace_real'] = df_strava['avg_speed_kmh'].apply(kmh_to_min_km)
 
 corridas = df_strava[cols]
-runs = corridas.loc[corridas['type'] == 'Run' ].sort_values(by='athlete.fullname')
-runs
+runs = corridas.loc[corridas['type'] == 'Run' ]
+
+# Agrupar por atleta e agregar as datas em uma lista
+grouped = runs.groupby("athlete.fullname").agg(
+    total_quilometros=("distance", lambda x: round(x.sum() / 1000, 2)),
+    
+    tempo_total=("moving_time_minutes", "sum"),
+    datas=("data_atual", lambda x: list(x))
+).reset_index()
+
+# Definir o intervalo de datas
+start_date = datetime.strptime('2025-01-08', '%Y-%m-%d')
+end_date = datetime.strptime('2025-01-18', '%Y-%m-%d')
+
+# Gerar uma lista de datas no intervalo
+date_range = [(start_date + timedelta(days=i)).strftime('%d-%m-%y') for i in range((end_date - start_date).days + 1)]
+
+# Verificar se cada data no intervalo está presente e criar colunas
+for date in date_range:
+    grouped[date] = grouped['datas'].apply(
+        lambda x: '🏃🏻‍♂️' if date in [datetime.strptime(d, '%Y-%m-%d').strftime('%d-%m-%y') for d in x] else ''
+    )
+
+# Verificar se o tempo_total de cada dia é maior que 30 minutos e adicionar 'tempo ok'
+for date in date_range:
+    grouped[date] = grouped.apply(
+        lambda row: f"{row[date]} ⏱️" if row[date] == '🏃🏻‍♂️' and row['tempo_total'] > 30 else row[date], axis=1
+    )
+
+# Remover a coluna 'datas'
+grouped = grouped.drop(columns=['datas'])
+# Ordenar por total_quilometros em ordem decrescente
+grouped = grouped.sort_values(by='total_quilometros', ascending=False)
+
+# Redefinir o índice e adicionar uma coluna de numeração começando de 1
+grouped.reset_index(drop=True, inplace=True)
+grouped.index += 1
+grouped.index.name = 'Classificação'
 
 
+# Renomear a coluna 'total_quilometros' para 'Distância'
+grouped.rename(columns={'total_quilometros': 'Distância'}, inplace=True)
+grouped.rename(columns={'athlete.fullname': 'Atleta'}, inplace=True)
+grouped.rename(columns={'tempo_total': 'Tempo Total'}, inplace=True)
+
+
+
+pontuacao_participantes = grouped
+
+pontuacao_participantes
+
+
+
+
+# In[ ]:
 
 
 
